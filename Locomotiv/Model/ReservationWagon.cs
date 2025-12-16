353e79d5c32318e38854bbca7dcbfb460fdd395f
Alpha-Trésor Ngendanduwme <ngendandumwea6@gmail.com>
2025-12-15 19:37:38 -0500

    Ajout des classes et enum de base pour une reservation de wagons

---
+++ Locomotiv/Model/ReservationWagon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Locomotiv.Model
+{
+    public class ReservationWagon
+    {
+        public int Id { get; set; }
+        public int ClientCommercialId { get; set; }
+        public User ClientCommercial { get; set; }
+
+        public int ItineraireId { get; set; }
+        public Itineraire Itineraire { get; set; }
+
+        public int NombreWagons { get; set; }
+        public TypeMarchandise TypeMarchandise { get; set; }
+        public double PoidsTotal { get; set; }
+        public double TarifTotal { get; set; }
+        public DateTime DateReservation { get; set; }
+        public StatutReservation Statut { get; set; }
+        public string? NotesSpeciales { get; set; }
+    }
+}
---
+++ Locomotiv/Model/TypeMarchandise.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Locomotiv.Model
+{
+    public enum TypeMarchandise
+    {
+        Standard,
+        Dangereux,
+        Perissable,
+        Fragile,
+        Surdimensionne
+    }
+}
