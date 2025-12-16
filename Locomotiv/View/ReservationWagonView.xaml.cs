5d64d9f120988366fcd6dd5ea6c8259802cd5042
Alpha-Trésor Ngendanduwme <ngendandumwea6@gmail.com>
2025-12-16 12:08:30 -0500

    Ajout des types pour le wagons et de la vue pour reservations

---
+++ Locomotiv/View/ReservationWagonView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Locomotiv.View
+{
+    /// <summary>
+    /// Logique d'interaction pour ReservationWagonView.xaml
+    /// </summary>
+    public partial class ReservationWagonView : Window
+    {
+        public ReservationWagonView()
+        {
+            InitializeComponent();
+        }
+    }
+}
