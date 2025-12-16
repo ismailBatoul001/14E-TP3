7e9bc912eefcf9f31fede670012f5ff6a4418fdb
Alpha-Trésor Ngendanduwme <ngendandumwea6@gmail.com>
2025-12-15 23:13:23 -0500

    Création du viewModel pour la reservation d'un wagon.

---
+++ Locomotiv/ViewModel/ReservationWagonViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Locomotiv.Model;
+using Locomotiv.Utils;
+using Locomotiv.Utils.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Locomotiv.ViewModel
+{
+    public class ReservationWagonViewModel : BaseViewModel
+    {
+        private readonly IReservationWagonService _reservationService;
+        private readonly IStationService _stationService;
+        private readonly IPlanificationItineraireService _itineraireService;
+        private readonly IUserSessionService _userSession;
+
+        private ObservableCollection<Station> _stations;
+    }
+}
